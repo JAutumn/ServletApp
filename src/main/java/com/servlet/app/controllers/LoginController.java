38512b53e9dce83816fbe8b43b64ba02ca595578
twister9893 <twister9893@yandex.com>
2016-09-18 16:01:45 +0400

    step-1.1

--- src/main/java/com/servlet/app/controllers/LoginController.java
+++ src/main/java/com/servlet/app/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.servlet.app.controllers;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,6 +12,22 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/")
 public class LoginController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("<html><body>Hello Servlet</body><html>");
+        resp.getWriter().write(getResponse(req));
+    }
+
+    private String getResponse(HttpServletRequest req) {
+        Enumeration<String> headerNames = req.getHeaderNames();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<html><body>");
+        while(headerNames.hasMoreElements()) {
+            String header = headerNames.nextElement();
+            stringBuilder.append("<div>")
+                         .append(header)
+                         .append(" : ")
+                         .append(req.getHeader(header))
+                         .append("</div>");
+        }
+        stringBuilder.append("</body><html>");
+        return stringBuilder.toString();
     }
 }
