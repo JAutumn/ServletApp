a248aa23f638981a4512814de1b2a98d43d5d67b
evgeniy <evgenij.filippov@simbirsoft.com>
2016-09-23 17:20:28 +0300

    remove unnecessary import

--- src/main/java/com/servlet/app/controllers/ErrorController.java
+++ src/main/java/com/servlet/app/controllers/ErrorController.java
@@ -10,8 +10,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.servlet.app.AppStarter;
-
 @WebServlet("/pages/error/")
 public class ErrorController extends HttpServlet {
 
